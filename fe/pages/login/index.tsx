0a830605884e85e7451478c1d7886ebc86e44579
anhpc1112 <phungconganh99prott@gmail.com>
2024-03-18 18:12:48 +0700

    add login page

---
+++ fe/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.css";
+
+export default function LoginPage() {
+    return (
+        <main className={styles.main}>
+            <h1>Login Page</h1>
+            <label>Email</label>
+            <input type="text" name="email"></input>
+            <label>Password</label>
+            <input type="password" name="password"></input>
+        </main>
+    );
+}
