838c87ecacd2a9d5a057ab5e367089b16a870954
anhpc1112 <phungconganh99prott@gmail.com>
2024-03-18 09:32:34 +0700

    add automatic cancel reversation

--- be/src/main/java/com/example/be/scheduled/CancelReservationAutomatic.java
+++ be/src/main/java/com/example/be/scheduled/CancelReservationAutomatic.java
@@ -3,10 +3,8 @@ package com.example.be.scheduled;
 import com.example.be.enums.SeatStatus;
 import com.example.be.model.Seat;
 import com.example.be.repository.SeatRepository;
-import com.example.be.service.ReservationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,33 +13,23 @@ import java.util.TimerTask;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CancelReservationAutomatic {
 
-    private boolean shouldRun = true;
-
-    @Autowired
-    private SeatRepository seatRepository;
+    private final SeatRepository seatRepository;
 
     @Scheduled(fixedDelay = 30000, initialDelay = 30000) // Cron expression for running every minute
     public void cancelReservation() {
-        if (shouldRun) {
-            Seat seat = seatRepository.findById(1L).orElseThrow(NullPointerException::new);
-            seat.setSeatStatus(SeatStatus.AVAILABLE);
-            seatRepository.save(seat);
-            log.info(String.format("Canceled seat with id: %s after 10 minutes", 1));
-            shouldRun = false;
-        }
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-
+                Seat seat = seatRepository.findById(1L).orElseThrow(NullPointerException::new);
+                seat.setSeatStatus(SeatStatus.AVAILABLE);
+                seatRepository.save(seat);
+                log.info(String.format("Canceled seat with id: %s after 10 minutes", 1));
             }
-        }, 10*60*1000);
-    }
-
-    public void activateTask() {
-        shouldRun = true;
+        }, (long) 10 * 60 * 1000);
     }
 }
