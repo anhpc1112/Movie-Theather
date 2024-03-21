d516d70af4c90004cc10e021a4fab54870006d2b
anhpc1112 <phungconganh99prott@gmail.com>
2024-03-21 16:35:34 +0700

    test login

--- be/src/main/java/com/example/be/controller/TestController.java
+++ be/src/main/java/com/example/be/controller/TestController.java
@@ -3,18 +3,20 @@ package com.example.be.controller;
 import com.example.be.service.impl.GoogleApiServiceHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/")
 public class TestController {
 
-    private final GoogleApiServiceHandler googleApiServiceHandlerl;
-
-    @GetMapping("/test")
-    public String authenticationGoogle() {
-        return googleApiServiceHandlerl.callGoogleApi();
+    @GetMapping
+    public Object authenticationGoogle(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        // http://localhost:3000/oauth2/redirect
+        return oAuth2AuthenticationToken.getPrincipal().getAttributes();
     }
 }
